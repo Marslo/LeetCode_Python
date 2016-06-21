559f60bf0bb0eece7750fade3372afb83eeec087
Marslo <marslo.jiao@gmail.com>
2016-06-21 15:08:16 +0800

    add the 1. Two Sum

---
+++ 1.TwoSum.py
@@ -0,0 +1,21 @@
+# =============================================================================
+#   FileName: 1.TwoSum.py
+#    Example:
+#       Given nums = [2, 7, 11, 15], target = 9,
+#       Because nums[0] + nums[1] = 2 + 7 = 9,
+#       return [0, 1].
+#     Author: Marslo
+#      Email: marslo.jiao@gmail.com
+#    Created: 2016-06-21 15:03:20
+#    Version: 0.0.1
+# LastChange: 2016-06-21 15:03:20
+# =============================================================================
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
